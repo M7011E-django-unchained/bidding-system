4e93368a278f16d3987f8c85f6e86636f0c07c47
casperlundberg <cappe5@hotmail.se>
2023-11-22 09:12:11 +0100

    new endpoints and smaller adjustments

--- README.md
+++ README.md
@@ -4,10 +4,10 @@ This is a bidding handler for the trading API created in Django, se more here: h
 
 # Installation
 
-## Prerequisites
+## Prerequisites (for dev mode)
 -  Node.js version 16 or newer
 
-## How to install
+## How to install (dev mode)
 
 Clone this repo into any folder. enter the `bidding-system` folder and run:
 ```bash
@@ -16,6 +16,17 @@ npm start
 ```
 
 This will install all required packages and then run the program.
+
+## For production
+Clone this repo into any folder. enter the `bidding-system` folder, create an .env file which should look like this:
+```
+DATABASE_URL = <db_url>
+TEST_DATABASE_URL = <db_url_for_testing_db>
+NODE_ENV = production
+```
+
+Then run `docker-compose up`. 
+
 
 # Note
 This is not supposed to be a full system for handling auctions or similar, just handling bids and bidding in another larger project. In other words, this system alone is not doing anything useful.
--- routes/routes.js
+++ routes/routes.js
@@ -30,10 +30,59 @@ router.get("/getAllBids", async (req, res) => {
   }
 });
 
+//Get all Bids by auctionId
+router.get("/getAllBidsByAuctionId/:auctionId", async (req, res) => {
+  try {
+    const data = await Model.find({
+      auctionId: req.params.auctionId,
+    });
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Get all Bids by bidderId
+router.get("/getAllBidsByBidderId/:bidderId", async (req, res) => {
+  try {
+    const data = await Model.find({
+      bidderId: req.params.bidderId,
+    });
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Get all Bids by bidderId and auctionId
+router.get("/getAllBids/:bidderId/:auctionId", async (req, res) => {
+  try {
+    const data = await Model.find({
+      bidderId: req.params.bidderId,
+      auctionId: req.params.auctionId,
+    });
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 //Get bid by ID
 router.get("/getOneBid/:id", async (req, res) => {
   try {
     const data = await Model.findById(req.params.id);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Get winner by auctionId, i.e the highest bidder
+router.get("/getWinnerByAuctionId/:auctionId", async (req, res) => {
+  try {
+    const data = await Model.findOne({ auctionId: req.params.auctionId }).sort({
+      bidAmount: -1,
+    });
     res.json(data);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -60,7 +109,20 @@ router.delete("/deleteOneBid/:id", async (req, res) => {
   try {
     const id = req.params.id;
     const data = await Model.findByIdAndDelete(id);
-    res.send(`Document with ${data.name} has been deleted..`);
+    res.send(`Bid with id = ${data._id} has been deleted..`);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+//Delete all bids by Auction ID
+router.delete("/deleteAllBidsByAuctionId/:auctionId", async (req, res) => {
+  try {
+    const auctionId = req.params.auctionId;
+    const data = await Model.deleteMany(auctionId);
+    res.send(
+      `All bids with on auction with auctionId = ${data.auctionId} has been deleted..`
+    );
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
