dc6376c527a36dac305c9ffc360e2bef40530ae1
casperlundberg <cappe5@hotmail.se>
2023-12-14 18:02:10 +0100

    ci/cd testing

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -29,74 +29,72 @@ describe(`GET /`, () => {
   });
 });
 
-if (process.env.NOT_TEST_GITHUB) {
-  describe("Middleware Setup", () => {
-    it("should pass the request to the next middleware if the token is valid", async () => {
-      // Mock the request and response objects
-      const url = process.env.DJANGO_API_TOKEN_URL;
-      const requestBody = {
-        username: process.env.DJANGO_API_SUPER_USER,
-        password: process.env.DJANGO_API_SUPER_USER_PASSWORD,
-      };
+describe("Middleware Setup", () => {
+  it("should pass the request to the next middleware if the token is valid", async () => {
+    // Mock the request and response objects
+    const url = process.env.DJANGO_API_TOKEN_URL;
+    const requestBody = {
+      username: process.env.DJANGO_API_SUPER_USER,
+      password: process.env.DJANGO_API_SUPER_USER_PASSWORD,
+    };
 
-      axios
-        .post(url, requestBody)
-        .then((response) => {
-          token = response.data.access;
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-
-      const req = {
-        headers: {
-          authorization: "Bearer " + token,
-          json: jest.fn(),
-        },
-      };
-      const res = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn(),
-      };
-
-      // Mock the next middleware function
-      const next = jest.fn();
-
-      // Call the middleware function
-      await authorizationMiddleware(req, res, next);
-
-      // Expect the next middleware to be called
-      //   expect(next.mock.calls).toHaveBeenCalled();
-    });
-
-    it("should return an error response if the token is invalid", async () => {
-      // Mock the request and response objects
-      const req = {
-        headers: {
-          authorization: "Bearer None",
-        },
-      };
-      const res = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn(),
-      };
-
-      // Mock the next middleware function
-      const next = jest.fn();
-
-      // Call the middleware function
-      await authorizationMiddleware(req, res, next);
-
-      // Expect the response status to be 500
-      expect(res.status).toHaveBeenCalledWith(500);
-
-      // Expect the response JSON to contain the error message
-      expect(res.json).toHaveBeenCalledWith({
-        message: "The format of the token is invalid",
+    axios
+      .post(url, requestBody)
+      .then((response) => {
+        token = response.data.access;
+      })
+      .catch((error) => {
+        console.error(error);
       });
 
-      // Expect the next middleware not to be called
-      expect(next).not.toHaveBeenCalled();
-    });
+    const req = {
+      headers: {
+        authorization: "Bearer " + token,
+        json: jest.fn(),
+      },
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    // Mock the next middleware function
+    const next = jest.fn();
+
+    // Call the middleware function
+    await authorizationMiddleware(req, res, next);
+
+    // Expect the next middleware to be called
+    //   expect(next.mock.calls).toHaveBeenCalled();
   });
-}
+
+  it("should return an error response if the token is invalid", async () => {
+    // Mock the request and response objects
+    const req = {
+      headers: {
+        authorization: "Bearer None",
+      },
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    // Mock the next middleware function
+    const next = jest.fn();
+
+    // Call the middleware function
+    await authorizationMiddleware(req, res, next);
+
+    // Expect the response status to be 500
+    expect(res.status).toHaveBeenCalledWith(500);
+
+    // Expect the response JSON to contain the error message
+    expect(res.json).toHaveBeenCalledWith({
+      message: "The format of the token is invalid",
+    });
+
+    // Expect the next middleware not to be called
+    expect(next).not.toHaveBeenCalled();
+  });
+});
